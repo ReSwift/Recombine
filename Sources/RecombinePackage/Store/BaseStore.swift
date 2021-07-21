9eab7445c15d81fcb78424bf7a6c3cb5d2f46ca4
Charles Maria Tor <charlotte@monadic.consulting>
2021-07-21 19:04:47 +1000

    Move usage of dispatch group to global queue

--- Sources/RecombinePackage/Store/BaseStore.swift
+++ Sources/RecombinePackage/Store/BaseStore.swift
@@ -52,37 +52,39 @@ public class BaseStore<State: Equatable, RawAction, RefinedAction>: StoreProtoco
 
         var group = Optional(DispatchGroup())
         group?.enter()
-        _preMiddlewareRefinedActions
-            .flatMap { [weak self] actions in
-                self.publisher()
-                    .flatMap { $0.$state.first() }
-                    .map { (actions, $0) }
-            }
-            .map { [weak self] actions, previousState in
-                actions.flatMap {
-                    middleware.transform(previousState, $0) { (actions: Action...) in
-                        self?.dispatch(actions: actions)
+        DispatchQueue.global().async {
+            self._preMiddlewareRefinedActions
+                .flatMap { [weak self] actions in
+                    self.publisher()
+                        .flatMap { $0.$state.first() }
+                        .map { (actions, $0) }
+                }
+                .map { [weak self] actions, previousState in
+                    actions.flatMap {
+                        middleware.transform(previousState, $0) { (actions: Action...) in
+                            self?.dispatch(actions: actions)
+                        }
                     }
                 }
-            }
-            .handleEvents(receiveOutput: { [weak self] in
-                self?._postMiddlewareRefinedActions.send($0)
-            })
-            .scan(state) { state, actions in
-                actions.reduce(state, reducer.reduce)
-            }
-            .prepend(state)
-            .receive(on: scheduler)
-            .sink { [weak self] state in
-                guard let self = self else { return }
-                self._allStateUpdates.send(state)
-                if self.state != state {
-                    self.state = state
+                .handleEvents(receiveOutput: { [weak self] in
+                    self?._postMiddlewareRefinedActions.send($0)
+                })
+                .scan(state) { state, actions in
+                    actions.reduce(state, reducer.reduce)
                 }
-                group?.leave()
-                group = nil
-            }
-            .store(in: &cancellables)
+                .prepend(state)
+                .receive(on: scheduler)
+                .sink { [weak self] state in
+                    guard let self = self else { return }
+                    self._allStateUpdates.send(state)
+                    if self.state != state {
+                        self.state = state
+                    }
+                    group?.leave()
+                    group = nil
+                }
+                .store(in: &self.cancellables)
+        }
         group?.wait()
     }
 
